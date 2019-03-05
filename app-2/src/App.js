dfe0d6d1e29eb74ea17b77bc11e3bceb79cf8cf1
Amir aghayi_chadegani <amiraghayi_chadegani@Amirs-iMac.local>
2019-03-05 01:17:29 -0700

    react-drills-update

--- app-2/src/App.js
+++ app-2/src/App.js
@@ -3,19 +3,42 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(){
+    super()
+
+    this.state = {
+       people: [
+          "Amir",
+          "Pari",
+          "Jalal",
+          "Sogol",
+          "Ramin",
+          "Ali",
+          "Mari"
+      ]
+    }
+
+
+
+  }
+
+
+
   render() {
+    let peopleToDisplay = this.state.people.map( (element, index) => {
+      return (
+        <h2 key={ index }>{ element }</h2>
+      )
+    })
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+         {peopleToDisplay}
       </div>
     );
   }
 }
+
+
+
 
 export default App;
--- app-4/src/App.js
+++ app-4/src/App.js
@@ -3,17 +3,71 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(){
+    super();
+
+    this.state = {
+      username: "",
+      password: ""
+    };
+
+    this.handleLogin = this.handleLogin.bind( this );
+
+  }
+
+
+  handleUsenameChange( name ){
+     this.setState({
+       username: name
+     });
+  }
+
+
+
+
+  handlePasswordChange(pass){
+     this.setState({
+       password: pass
+     });
+  }
+
+
+  handleLogin(){
+    console.log(`Logging in with username: ${ this.state.username} and password: ${ this.state.password }`);
+ }
+
+
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+        <div className="App">
+
+          <br />
+
+            <div>
+              <h1>Username: 
+                <input
+                type="text"
+                onChange={ (e) => this.handleUsenameChange(e.target.value) }
+                /> 
+              </h1>
+            </div>
+
+            <div>
+              <h1>Password: 
+                <input 
+                type="password"
+                onChange={ (e) => this.handlePasswordChange(e.target.value) }
+                />
+              </h1>
+            </div>
+
+            <div>
+              <button
+              onClick={this.handleLogin }
+              >Login</button>
+            </div>
+
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
     );
   }
 }
